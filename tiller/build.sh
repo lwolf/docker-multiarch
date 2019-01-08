36d2dfee94320a8e8a72d713e61040e3791cd520
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-01-08 23:45:09 +0200

    optimising build.sh in tiller

--- tiller/build.sh
+++ tiller/build.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 export GITHUB_REPO=helm/helm
 export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
+export DOCKER_REPO=lwolf/helm
 
 mkdir -p /tmp/release
 for ARCH_TARGETS in amd64_amd64 arm64_arm64v8 arm_arm32v6
@@ -12,52 +13,38 @@ do
     ARCH=${ARCH_TARGETS%%_*} # will drop part of string from first occur of `SubStr` to the end
     wget -O- https://storage.googleapis.com/kubernetes-helm/helm-${VERSION}-linux-${ARCH}.tar.gz | tar xvz
     cp linux-${ARCH}/{tiller,helm} .
-    docker build -t lwolf/helm:${VERSION}-${ARCH} --build-arg target=${TARGET} .
-    docker push lwolf/helm:${VERSION}-${ARCH}
+    docker build -t ${DOCKER_REPO}:${VERSION}-${ARCH} --build-arg target=${TARGET} .
+    docker push ${DOCKER_REPO}:${VERSION}-${ARCH}
     rm -Rf {tiller,helm} linux-${ARCH}
 done
 
 docker manifest create --amend \
-    lwolf/helm:${VERSION} \
-    lwolf/helm:${VERSION}-amd64 \
-    lwolf/helm:${VERSION}-arm64 \
-    lwolf/helm:${VERSION}-arm
-
-docker manifest annotate \
-    lwolf/helm:latest \
-    lwolf/helm:${VERSION}-amd64 \
-    --os linux --arch amd64
-
-docker manifest annotate \
-    lwolf/helm:latest \
-    lwolf/helm:${VERSION}-arm64 \
-    --os linux --arch arm64
-
-docker manifest annotate \
-    lwolf/helm:latest \
-    lwolf/helm:${VERSION}-arm \
-    --os linux --arch arm
+    ${DOCKER_REPO}:${VERSION} \
+    ${DOCKER_REPO}:${VERSION}-amd64 \
+    ${DOCKER_REPO}:${VERSION}-arm64 \
+    ${DOCKER_REPO}:${VERSION}-arm
 
 docker manifest create --amend \
-    lwolf/helm:latest \
-    lwolf/helm:${VERSION}-amd64 \
-    lwolf/helm:${VERSION}-arm64 \
-    lwolf/helm:${VERSION}-arm
+    ${DOCKER_REPO}:latest \
+    ${DOCKER_REPO}:${VERSION}-amd64 \
+    ${DOCKER_REPO}:${VERSION}-arm64 \
+    ${DOCKER_REPO}:${VERSION}-arm
 
-docker manifest annotate \
-    lwolf/helm:${VERSION} \
-    lwolf/helm:${VERSION}-amd64 \
-    --os linux --arch amd64
+for OS_ARCH in linux_amd64 linux_arm linux_arm64
+do
+    OS=${ARCH_TARGETS#*_}
+    ARCH=${ARCH_TARGETS%%_*}
 
-docker manifest annotate \
-    lwolf/helm:${VERSION} \
-    lwolf/helm:${VERSION}-arm64 \
-    --os linux --arch arm64
+    docker manifest annotate \
+        ${DOCKER_REPO}:${VERSION} \
+        ${DOCKER_REPO}:${VERSION}-${ARCH} \
+        --os ${OS} --arch ${ARCH}
 
-docker manifest annotate \
-    lwolf/helm:${VERSION} \
-    lwolf/helm:${VERSION}-arm \
-    --os linux --arch arm
+    docker manifest annotate \
+        ${DOCKER_REPO}:latest \
+        ${DOCKER_REPO}:${VERSION}-${ARCH} \
+        --os ${OS} --arch ${ARCH}
+done
 
-docker manifest push lwolf/helm:${VERSION}
-docker manifest push lwolf/helm:latest
+docker manifest push ${DOCKER_REPO}:${VERSION}
+docker manifest push ${DOCKER_REPO}:latest
