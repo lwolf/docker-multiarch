ac78f30e8ea102147d146f9def4e6433b95476f1
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-01-08 23:46:35 +0200

    add kubernetes-dashboard build script

---
+++ kubernetes-dashboard/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export GITHUB_REPO=kubernetes/dashboard
+export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
+export DOCKER_REPO=lwolf/kubernetes-dashboard
+
+for ARCH in amd64 arm64 arm
+do
+    docker pull k8s.gcr.io/kubernetes-dashboard-${ARCH}:${VERSION}
+    docker tag k8s.gcr.io/kubernetes-dashboard-${ARCH}:${VERSION} ${DOCKER_REPO}:${VERSION}-${ARCH}
+    docker push ${DOCKER_REPO}:${VERSION}-${ARCH}
+done
+
+docker manifest create --amend \
+    ${DOCKER_REPO}:${VERSION} \
+    ${DOCKER_REPO}:${VERSION}-amd64 \
+    ${DOCKER_REPO}:${VERSION}-arm64 \
+    ${DOCKER_REPO}:${VERSION}-arm
+
+docker manifest create --amend \
+    ${DOCKER_REPO}:latest \
+    ${DOCKER_REPO}:${VERSION}-amd64 \
+    ${DOCKER_REPO}:${VERSION}-arm64 \
+    ${DOCKER_REPO}:${VERSION}-arm
+
+for OS_ARCH in linux_amd64 linux_arm linux_arm64
+do
+    OS=${OS_ARCH%%_*}
+    ARCH=${OS_ARCH#*_}
+
+    docker manifest annotate \
+        ${DOCKER_REPO}:${VERSION} \
+        ${DOCKER_REPO}:${VERSION}-${ARCH} \
+        --os ${OS} --arch ${ARCH}
+
+    docker manifest annotate \
+        ${DOCKER_REPO}:latest \
+        ${DOCKER_REPO}:${VERSION}-${ARCH} \
+        --os ${OS} --arch ${ARCH}
+done
+
+docker manifest push ${DOCKER_REPO}:${VERSION}
+docker manifest push ${DOCKER_REPO}:latest
