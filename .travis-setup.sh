94c81868891e16655b172bb60887425e876c5348
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-01-16 18:20:09 +0000

    test quemu

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -32,6 +32,7 @@ setup_dependencies() {
 
   sudo apt update -y
   sudo apt install -y jq curl
+  sudo apt install -y --no-install-recommends binfmt-support qemu-user-static
   sudo apt install --only-upgrade docker-ce -y
 
   docker info
