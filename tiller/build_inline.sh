03fa903e64956d3f3a8795fa4a938611d7c753f3
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-01-16 22:08:56 +0000

    test

--- tiller/build_inline.sh
+++ tiller/build_inline.sh
@@ -14,6 +14,10 @@ export DOCKER_REPO=lwolf/helm
 # Get QEMU
 curl -sL -o qemu-${QEMU_ARCH}-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/qemu-${QEMU_ARCH}-static.tar.gz && tar zx -f qemu-${QEMU_ARCH}-static.tar.gz
 
+wget -O- https://storage.googleapis.com/kubernetes-helm/helm-${VERSION}-linux-${ARCH}.tar.gz | tar xvz
+cp linux-${ARCH}/{tiller,helm} .
+
+
 # Build image
 # docker run --rm --privileged multiarch/qemu-user-static:register
 cp Dockerfile.arm64 Dockerfile
