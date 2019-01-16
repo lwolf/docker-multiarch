37e65ef9479cf3954892ccc30d54e4a71114d01c
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-01-16 21:20:52 +0000

    test

--- tiller/build.sh
+++ tiller/build.sh
@@ -7,13 +7,11 @@ export GITHUB_REPO=helm/helm
 export VERSION=v2.12.2
 export DOCKER_REPO=lwolf/helm
 
-mkdir -p /tmp/release
-
 for ARCH in amd64 arm64 arm
 do
     wget -O- https://storage.googleapis.com/kubernetes-helm/helm-${VERSION}-linux-${ARCH}.tar.gz | tar xvz
     cp linux-${ARCH}/{tiller,helm} .
-    docker build -t ${DOCKER_REPO}:${VERSION}-${ARCH} -f Dockerfile.${ARCH} .
+    docker build -t ${DOCKER_REPO}:${VERSION}-${ARCH} -f tiller/Dockerfile.${ARCH} .
     docker push ${DOCKER_REPO}:${VERSION}-${ARCH}
     rm -Rf {tiller,helm} linux-${ARCH}
 done
