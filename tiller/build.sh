4c65e2c3546643635d8a13d759c3e02a30483ca7
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-01-17 07:58:48 +0000

    test

--- tiller/build.sh
+++ tiller/build.sh
@@ -7,9 +7,55 @@ export GITHUB_REPO=helm/helm
 export VERSION=v2.12.2
 export DOCKER_REPO=lwolf/helm
 
-bash build.arm.sh
-bash build.arm64.sh
-bash build.amd64.sh
+for ARCH in amd64 arm64 arm
+do
+    if [ "$ARCH" == "amd64" ];then
+        export QEMU_VERSION=v3.1.0-2
+        export export GITHUB_REPO=helm/helm
+        export TARGET=amd64
+        export QEMU_ARCH=x86_64
+        export ARCH=amd64
+        export VERSION=v2.12.2
+        export DOCKER_REPO=lwolf/helm
+    elif [ "$ARCH" == "arm" ]; then
+        export QEMU_VERSION=v3.1.0-2
+        export export GITHUB_REPO=helm/helm
+        export TARGET=arm32v6
+        export QEMU_ARCH=arm
+        export ARCH=arm
+        export VERSION=v2.12.2
+        export DOCKER_REPO=lwolf/helm
+    elif [ "$ARCH" == "arm64" ]; then
+        export QEMU_VERSION=v3.1.0-2
+        export export GITHUB_REPO=helm/helm
+        export TARGET=arm64v8
+        export QEMU_ARCH=aarch64
+        export ARCH=arm64
+        export VERSION=v2.12.2
+        export DOCKER_REPO=lwolf/helm
+    else
+        echo "unknown architecture type"
+        return 1
+    fi
+
+    # Get QEMU
+    curl -sL -o qemu-${QEMU_ARCH}-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/qemu-${QEMU_ARCH}-static.tar.gz && tar zx -f qemu-${QEMU_ARCH}-static.tar.gz
+
+    wget -O- https://storage.googleapis.com/kubernetes-helm/helm-${VERSION}-linux-${ARCH}.tar.gz | tar xvz
+    cp linux-${ARCH}/{tiller,helm} .
+
+    # Build image
+    docker build -t $DOCKER_REPO:${VERSION}-${ARCH}  --build-arg target=${TARGET} --build-arg qemu_arch=${QEMU_ARCH} .
+
+    # Push image
+    docker push ${DOCKER_REPO}:${VERSION}-${ARCH}
+
+
+done
+
+# bash build.arm.sh
+# bash build.arm64.sh
+# bash build.amd64.sh
 
 docker manifest create --amend \
     ${DOCKER_REPO}:${VERSION} \
