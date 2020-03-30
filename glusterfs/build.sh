cbdb5e26f3062ba0e28729c4569dd41628a9cffc
Sergey Nuzhdin <ipaq.lw@gmail.com>
2020-03-30 18:06:11 +0100

    glustefs7 docker image
    Signed-off-by: Sergey Nuzhdin <ipaq.lw@gmail.com>

---
+++ glusterfs/build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export QEMU_VERSION=v3.1.0-2
+export VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://launchpad.net/~gluster/+archive/ubuntu/glusterfs-7 | grep "ubuntu1~focal1" | grep -oE '[0-9].[0-9]')
+export DOCKER_REPO=lwolf/glusterfs
+
+docker manifest inspect ${DOCKER_REPO}:${VERSION} > /dev/null && echo "Version ${VERSION} is already exists" && exit 0
+
+for ARCH_TYPE in amd64 arm64 arm
+do
+    if [ "$ARCH_TYPE" == "amd64" ];then
+        export TARGET=amd64
+        export QEMU_ARCH=x86_64
+        export ARCH=amd64
+    elif [ "$ARCH_TYPE" == "arm" ]; then
+        export TARGET=arm32v6
+        export QEMU_ARCH=arm
+        export ARCH=arm
+    elif [ "$ARCH_TYPE" == "arm64" ]; then
+        export TARGET=arm64v8
+        export QEMU_ARCH=aarch64
+        export ARCH=arm64
+    else
+        echo "unsupported architecture type"
+        return 1
+    fi
+
+    # Get QEMU
+    curl -sL -o qemu-${QEMU_ARCH}-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/qemu-${QEMU_ARCH}-static.tar.gz && tar zx -f qemu-${QEMU_ARCH}-static.tar.gz
+
+    # Build image
+    docker build -t $DOCKER_REPO:${VERSION}-${ARCH} --build-arg target=${TARGET} --build-arg qemu_arch=${QEMU_ARCH} .
+
+    # Push image
+    docker push ${DOCKER_REPO}:${VERSION}-${ARCH}
+
+    rm -Rf cni-plugins
+
+done
+
+docker manifest create --amend \
+    ${DOCKER_REPO}:${VERSION} \
+    ${DOCKER_REPO}:${VERSION}-amd64 \
+    ${DOCKER_REPO}:${VERSION}-arm64 \
+    ${DOCKER_REPO}:${VERSION}-arm
+
+for OS_ARCH in linux_amd64 linux_arm64
+do
+    ARCH=${OS_ARCH#*_}
+    OS=${OS_ARCH%%_*}
+
+    docker manifest annotate \
+        ${DOCKER_REPO}:${VERSION} \
+        ${DOCKER_REPO}:${VERSION}-${ARCH} \
+        --os ${OS} --arch ${ARCH}
+done
+
+docker manifest annotate \
+    ${DOCKER_REPO}:${VERSION} \
+    ${DOCKER_REPO}:${VERSION}-arm \
+    --os linux --arch arm --variant v6
+
+docker manifest push ${DOCKER_REPO}:${VERSION}
